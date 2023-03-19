bea97ebfdb318041529ad64a10afe2774a2bae2c
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2023-03-19 21:03:57 +0200

    added initial image creation before train

--- src/main.py
+++ src/main.py
@@ -85,8 +85,10 @@ gen_optimizer = torch.optim.Adam(gen_model.parameters(), lr=GEN_LEARNING_RATE, b
 # == Train loop ==
 epoch_losses = pd.DataFrame(columns=['epoch', 'gen_loss', 'dis_loss'])
 fixed_noise = torch.randn(64, LATENT_DIM, 1, 1, device=device)
+sample_from_generator(64, gen_model, device, title=f'Epoch 0', path_to_save=output_path / Path(f'epoch_0'),
+                      noise=fixed_noise)
 
-for epoch in range(NUM_EPOCHS):
+for epoch in range(NUM_EPOCHS)+1:
     batch_losses_gen = []
     batch_losses_dis = []
 
